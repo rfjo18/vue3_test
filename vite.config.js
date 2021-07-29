25034bf5af75969cf690aee0998f9a9de02e437c
chenbaochuan <930056064@qq.com>
2021-07-29 17:22:42 +0800

    更改

--- vite.config.js
+++ vite.config.js
@@ -20,10 +20,14 @@ export default defineConfig({
             additionalData: `$injectedColor: orange;`
         }*/
     },
+    build: {
+        //brotliSize: false, // 默认为 true
+        outDir:'./docs',
+    },
     /*server: {
         // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
         host: '0.0.0.0',
-        port: 56438, // 服务器端口号
+        port: 56438, // 服务器端口号 docs
         open: false, // 是否自动打开浏览器
         // 代理
         proxy: {
