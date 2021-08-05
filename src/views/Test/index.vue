206efa56052886eb83982ff0b38758d65f8fe4a2
chenbaochuan <930056064@qq.com>
2021-08-05 18:13:13 +0800

    更改

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -4,14 +4,53 @@
             <MyLi></MyLi>
             <MyLi></MyLi>
         </MyUl>
-    <el-select value="as">
+    <el-select >
         <el-option
             label="item.label"
             value="item.value">asd</el-option>
     </el-select>
 
-
+        <el-collapse accordion>
+            <el-collapse-item>
+                <template #title>
+                    一致性 Consistency<i class="el-collapse-item__arrow el-icon-arrow-right"></i>
+                </template>
+                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+            </el-collapse-item>
+            <el-collapse-item title="反馈 Feedback">
+                <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+            </el-collapse-item>
+            <el-collapse-item title="效率 Efficiency">
+                <div>简化流程：设计简洁直观的操作流程；</div>
+                <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+                <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+            </el-collapse-item>
+            <el-collapse-item title="可控 Controllability">
+                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当zhonyuznaodaole shibushiz是不是这个说法福不对好不好div《div》珀斯头呢position八艘利特
+                    relative 掉啦有display block 心理呢-block前操作等。</div>
+            </el-collapse-item>
+        </el-collapse>
     </div>
+
+    <div   class="asdas">
+        <el-dialog append-to-body v-dialogDrag
+                   title="提示"
+                   v-model="dialogVisible"
+                   width="30%">
+            <span>这是一段信息</span>
+            <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="dialogVisible = false">取 消</el-button>
+              <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+            </template>
+        </el-dialog>
+    </div>
+
+    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
 </template>
 
 <script>
@@ -22,7 +61,9 @@ import MyLi from "./MyLi.vue";
         name: "Test",
         components: {MyUl,MyLi},
         data() {
-            return {}
+            return {
+                dialogVisible:false
+            }
         },
         computed: {},
         created() {
